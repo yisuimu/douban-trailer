f77a0ec76660b8dcb8f2d8dde16f3f7db266899e
banyangyang <banyangyang@58ganji.com>
2018-06-11 17:57:50 +0800

    html

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const app = new Koa()
+const { normal }  = require('./tpl')
+
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html;charset=utf-8;'
+    ctx.body = normal
+})
+app.listen(4455)
---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css">
+        <script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.bundle.js"></script>
+        <script src="https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js"></script>
+        <title>koa server HTML</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1>hi luke</h1>
+                    <p>this is yangyang</p>
+                </div>
+                <div class="col-md-4">
+                    <p>hello world</p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+`
