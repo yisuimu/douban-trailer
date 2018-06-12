17a8b8ae893f571a4d69b6633aa7a6b5c088d952
banyangyang <banyangyang@58ganji.com>
2018-06-12 15:09:45 +0800

    增加魔板引擎中间价

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl }  = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+// const { htmlTpl, ejsTpl, pugTpl }  = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8;'
-    ctx.body = pug.render(pugTpl, {
+    // ctx.type = 'text/html;charset=utf-8;'
+    // ctx.body = pug.render(pugTpl, {
+    //     you: 'luke',
+    //     me: 'yangyang'
+    // })
+    await ctx.render('index', {
         you: 'luke',
         me: 'yangyang'
     })
